0348da9538fe9fa26fc9600f2b932f20dbe0a6a7
Kaarthik <101412623+Kart-95@users.noreply.github.com>
2025-09-06 12:11:53 +0530

    Update Axios.js

--- frontend/atts/src/api/Axios.js
+++ frontend/atts/src/api/Axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:3000/api"});
+const api = axios.create({ baseURL: "https://atts-metalrate.onrender.com"});
 
 export default api;
