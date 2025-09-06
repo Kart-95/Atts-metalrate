b0933954c7ca45697a1270fdafeca48aa23332e7
Kaarthik <101412623+Kart-95@users.noreply.github.com>
2025-09-06 13:41:59 +0530

    Update RateApi.js

--- frontend/atts/src/api/RateApi.js
+++ frontend/atts/src/api/RateApi.js
@@ -1,8 +1,26 @@
 import axios from "axios";
 
+
 const API_URL = "https://atts-metalrate.onrender.com/api/rates";
 
-export const getRates = () => axios.get(API_URL);
-export const getRatesByMetalAndPurity = (metal, purity) =>
-  axios.get(`${API_URL}?metal=${metal}&purity=${purity}`);
-export const createRate = (data) => axios.post(API_URL, data);
+
+export const getRates = async () => {
+  const res = await axios.get(API_URL);
+  return res.data;
+};
+
+
+export const addRate = async (rateData) => {
+  const res = await axios.post(API_URL, rateData);
+  return res.data;
+};
+
+
+export const getLatestRate = async (metal, purity) => {
+  const params = {};
+  if (metal) params.metal = metal;
+  if (purity) params.purity = purity;
+
+  const res = await axios.get(`${API_URL}/latest`, { params });
+  return res.data;
+};
